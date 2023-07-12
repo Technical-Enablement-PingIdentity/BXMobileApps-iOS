6012917efffc3e8c5724ec30a0645f3cdc038099
Eric Anderson <ericanderson@pingidentity.com>
2023-07-12 09:59:01 -0600

    Initial Commit

---
+++ BXMobileApps-iOS/BXMobileApps_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BXMobileApps_iOSApp.swift
+//  BXMobileApps-iOS
+//
+//  Created by Eric Anderson on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BXMobileApps_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
